94048602ac2bf120192f31fffa1dd873d968a394
Zack Enders <enders.zack@gmail.com>
2018-03-08 11:50:34 -0500

    Added comments to the code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
     protected
-
+      # Costum devise parameters allows the user to add a name a sign up
         def configure_permitted_parameters
             devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password, :password_confirmation]) 
             devise_parameter_sanitizer.permit(:account_update, keys: [:name, :is_female, :date_of_birth, :email, :password, :password_confirmation, :current_password]) 
@@ -17,6 +17,8 @@ class ApplicationController < ActionController::Base
   		def after_sign_out_path_for(resource_or_scope)
     			root_path
  		  end
+
+        # Overwriting the sign_in redirect path method
       def after_sign_in_path_for(resource_or_scope)
           "/users/#{current_user.id}"
       end
