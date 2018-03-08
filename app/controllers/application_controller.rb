2f4b0bab049b7adf3d15b90bd7a5bc69dfbd8135
Zack Enders <enders.zack@gmail.com>
2018-03-07 20:25:37 -0500

    Fixed an issue where the name wasnt being saved after signing up for the site

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
     protected
 
         def configure_permitted_parameters
-            devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password, :password_confirmation]) 
+            devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password, :password_confirmation]) 
             devise_parameter_sanitizer.permit(:account_update, keys: [:name, :is_female, :date_of_birth, :email, :password, :password_confirmation, :current_password]) 
         end
 
